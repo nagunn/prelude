9452bafedebe6628d2ce481ed595ea9577e630fd
Nick Gunn <nick@ausimian.net>
2015-10-21 21:37:27 +1100

    Ensure package archives are present.

--- personal/preload/my-theme-init.el
+++ personal/preload/my-theme-init.el
@@ -6,11 +6,17 @@
 
 ;;; Noctilux is a good theme on all platforms.
 (require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (package-initialize)
+
+(unless package-archive-contents
+  (package-refresh-contents))
+
 (unless (package-installed-p 'noctilux-theme)
   (package-install 'noctilux-theme))
+
 (custom-set-variables
  '(prelude-theme 'noctilux))
 
-(provide 'my-preload)
+(provide 'my-theme-init)
 ;;; my-preload.el ends here
