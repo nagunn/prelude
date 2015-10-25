34e30e96f8720aac342748434efeb3c25bed1ae7
Nick Gunn <nick@ausimian.net>
2015-10-25 15:19:22 +1100

    Incorporate eyebrowse to manage windows layouts.

--- personal/my-real-estate.el
+++ personal/my-real-estate.el
@@ -3,8 +3,14 @@
 ;;; Turn off various real-estate hogs
 
 ;;; Code:
+(declare-function prelude-require-package "prelude-packages.el")
+
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
+
+;;; Remember window loyouts
+(prelude-require-package 'eyebrowse)
+(eyebrowse-mode t)
 
 (provide 'my-real-estate)
 ;;; my-real-estate.el ends here
