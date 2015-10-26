ef34ae255d0c0121ff2e4e9e550d6cb9bb163f03
Nick Gunn <nick@ausimian.net>
2015-10-26 19:18:26 +1100

    Added frame and window navigation.

--- personal/my-real-estate.el
+++ personal/my-real-estate.el
@@ -12,5 +12,9 @@
 (prelude-require-package 'eyebrowse)
 (eyebrowse-mode t)
 
+;;; Frame and window navigation
+(prelude-require-package 'ace-window)
+(global-set-key (kbd "M-p") 'ace-window)
+
 (provide 'my-real-estate)
 ;;; my-real-estate.el ends here
