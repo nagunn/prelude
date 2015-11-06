aaa03978cb6ea7b4862fa9e209622ec569f57ad7
unknown <nick@ausimian.net>
2015-11-06 13:00:56 +1100

    Fixes to org-publish support for blog.

--- personal/my-blog.el
+++ personal/my-blog.el
@@ -14,27 +14,38 @@
 
 ;;; Code:
 (require 'ox-publish)
-(let* ((blog-dir (file-name-as-directory (getenv "BLOG_DIR")))
-       (blog-source (file-name-as-directory (concat blog-dir "source")))
-       (blog-master (file-name-as-directory (concat blog-dir "master"))))
-  (setq org-publish-project-alist
-        `(
-          ("org-notes"
-           :base-directory ,blog-source
-           :base-extension "org"
-           :exclude "template.org"
-           :publishing-directory ,blog-master
-           :recursive t
-           :publishing-function org-html-publish-to-html
-           :headline-levels 4
-           :auto-preamble t)
-          ("org-static"
-           :base-directory ,blog-source
-           :base-extension "css"
-           :publishing-directory ,blog-master
-           :recursive t
-           :publishing-function org-publish-attachment)
-          ("org" :components ("org-notes" "org-static")))))
+
+(defun my-blog-footer ()
+  "Return the footer for the blog."
+  (concat "<footer><p>Copyright (C) " (user-full-name) " " (format-time-string "%Y" (current-time)) ".</p>"
+          "<p>Powered by <a href=\"http://orgmode.org\">Org mode</a>.</p></footer>"))
+
+
+(when (getenv "BLOG_DIR")
+  (let* ((blog-dir (file-name-as-directory (getenv "BLOG_DIR")))
+         (blog-source (file-name-as-directory (concat blog-dir "source")))
+         (blog-master (file-name-as-directory (concat blog-dir "master"))))
+    (setq org-publish-project-alist
+          `(
+            ("org-notes"
+             :base-directory ,blog-source
+             :base-extension "org"
+             :exclude "template.org"
+             :publishing-directory ,blog-master
+             :recursive t
+             :publishing-function org-html-publish-to-html
+             :headline-levels 4
+             :auto-preamble t)
+            ("org-static"
+             :base-directory ,blog-source
+             :base-extension "css"
+             :publishing-directory ,blog-master
+             :recursive t
+             :publishing-function org-publish-attachment)
+            ("org" :components ("org-notes" "org-static")))))
+
+  (setq org-html-htmlize-output-type 'css)
+  (setq org-html-postamble-format `(("en" ,(my-blog-footer)))))
 
 (provide 'my-blog)
 ;;; my-blog.el ends here
--- personal/my-details.el
+++ personal/my-details.el
@@ -6,9 +6,7 @@
 (require 'package)
 (require 'magit)
 
-(when (and (executable-find "git") (package-installed-p 'magit))
-  (setq user-mail-address (magit-get "user.email"))
-  (setq user-full-name (magit-get "user.name")))
+(setq user-mail-address (getenv "EMAIL"))
 
 (provide 'my-details)
 ;;; my-details.el ends here
