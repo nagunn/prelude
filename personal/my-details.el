3ad181cc453ff06d572a3a050ccfae13daa13227
Nick Gunn <nick@ausimian.net>
2015-11-08 14:37:44 -0800

    Separate email account from address.

--- personal/my-details.el
+++ personal/my-details.el
@@ -5,6 +5,7 @@
 ;;; Code:
 (setq user-full-name    (getenv "USER"))
 (setq user-mail-address (getenv "EMAIL"))
+(setq user-mail-account (getenv "EMAILACCOUNT"))
 
 (provide 'my-details)
 ;;; my-details.el ends here
