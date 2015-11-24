968c38007be9be0b88cfed06edd4d12f792c4010
Nick Gunn <nick@ausimian.net>
2015-11-25 09:20:18 +1100

    Added multiple-cursor support in evil-mode.

---
+++ personal/my-evil-cursors.el
@@ -0,0 +1,9 @@
+;;; my-evil-cursors.el --- multiple-cursors that works in evil-mode.
+;;; Commentary:
+;;; See https://github.com/gabesoft/evil-mc
+
+;;; Code:
+(prelude-require-package 'evil-mc)
+
+(provide 'my-evil-cursors)
+;;; my-evil-cursors.el ends here
